0a82fde331e831371703d4acb44f1bd3bfef7286
Your Name <you@example.com>
2020-04-06 17:05:33 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+
+  # get 'articles/edit/:id', to: 'articles#edit'
+  # patch 'articles/:id', to: 'articles#update'
 end
